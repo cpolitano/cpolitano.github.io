97e7547fcc2eedbed7d0e3bf87dc8acdd31b5924
cpolitano <cpolitano8@gmail.com>
2014-12-23 11:51:17 -0500

    update copy, js, and added animation

--- main.js
+++ main.js
@@ -1,19 +1,7 @@
-$(document).ready( function() {
-	
-	$('nav a').on("click", function() {
-		event.preventDefault();		
-		if ( $(this).hasClass("active") ) {
-			$("#slide-down").slideUp(1000).removeClass("open");
-			$(this).removeClass("active");
-			$('#intro').fadeIn(1500);	
-		}
-		else {
-			$('nav a').removeClass("active"); 
-			$('#intro').fadeOut(1500);	
-			var thisClass = $(this).attr("class");
-			$(this).toggleClass("active");					
-			$("#slide-down").slideDown(1000).addClass("open");
-			$('#' + thisClass).toggleClass("open").show().siblings().removeClass("open").hide();		
-		};		
-	});
-})
+var icon = document.getElementById('icon');
+var anchor = document.getElementById('about--anchor');
+
+icon.addEventListener('click', function(event){
+  event.preventDefault();
+  Velocity(anchor, "scroll", { duration: 500 }, { easing: "ease-in-out" });
+});
